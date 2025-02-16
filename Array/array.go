e8771d2ff7705c61b95071c976f24fa0c6cd8c69
Sujan Timalsina <sujantimalsina333@gmail.com>
2025-02-16 20:34:31 +0545

    Array in Golang

---
+++ Array/array.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("We are learning about Array in Go Language")
+
+	// var name[5] string
+	// name[0] = "Alice"
+	// name[2] = "Bob"
+	// fmt.Println("Names of Person is :",name)
+
+	// numbers := [5]int{1, 2, 3, 4, 5}
+	// fmt.Println(numbers)
+	// fmt.Println("Length of the array is :", len(numbers))
+	// fmt.Println("Value of number at 2nd index:",numbers[2])
+
+
+	var price[5]string
+	fmt.Println("Price is :",price)
+	fmt.Printf("Value :%q",price)
+}
